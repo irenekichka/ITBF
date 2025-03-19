91869dab1b69d305fd2ea352358603419beb7a77
irenekichka <irene.kichka@gmail.com>
2025-03-19 22:09:32 +0300

    fix 1

--- hw_1/main.py
+++ hw_1/main.py
@@ -1,31 +1,47 @@
 """
-Домашнее задание №1
-Функции и структуры данных
+Домашнее задание №1. Функции и структуры данных
 """
- """
-    функция, которая принимает N целых чисел,
+"""
+    Функция, которая принимает N целых чисел, 
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
-    """
+"""    
+def power_numbers(input):
+    input = input.split(', ')
+    return [int(x) ** 2 for x in input]
     
-def power_numbers(n):
-    for a in range(0, len(n)):
-        n[a] = n[a]**2
-        print(n)
-power_numbers ([1, 2, 5, 7])
+print(power_numbers('1, 2, 4'))
 
-
-    """
-    функция, которая на вход принимает список из целых чисел,
+    
+"""
+    функция, которая на вход принимает список из целых чисел, 
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
-
     >>> filter_numbers([1, 2, 3], ODD)
     <<< [1, 3]
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
-    """
+"""    
+def filter_numbers(x, str):
+    ODD = "odd"
+    EVEN = "even"
+    PRIME = "prime"
+    c = []
+    if str == ODD:
+        c = filter(odd, x)
+    if str == EVEN:
+        c = filter(even, x)
+    if str == PRIME:
+        c = filter(prime, x)
+    return list(c)
+
+def even(n):
+    return n % 2 == 0
+    
+def odd(n):
+    return n % 2 != 0
+    
 def prime(a):
     if a == 1: return False
     test = True
@@ -37,28 +53,6 @@ def prime(a):
         b = b - 1
     return a if test else False
     
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
-
-def filter_numbers(x, str):
-    c = []
-    if str == ODD:
-        for i in x:
-            if i%2!=0:
-                c.append(i)
-    if str == EVEN:
-        for i in x:
-            if i%2==0:
-                c.append(i)
-    if str == PRIME:
-        for i in x:
-            if prime(i):
-                c.append(i)
-    print(c)
-
-x = [1, 2, 3, 4, 5]
-filter_numbers(x, PRIME)
-
-
-
+print(filter_numbers([1, 2, 3, 4, 5], ODD))
+print(filter_numbers([1, 2, 3, 4, 5], EVEN))
+print(filter_numbers([1, 2, 3, 4, 5], PRIME))
